9857dc185762341a7eeb0356699b2ed428c0e4d3
Joshua Schoonover <joshua.schoonover@austincc.edu>
2024-10-22 15:59:33 -0400

    line2

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,19 +16,32 @@ export default function Timeline(TimelineProps:TimelineProps){
                 lineStyle={{borderColor:'black'}}
                 >
                 <TLEvent
-                    title='1999'
-                    body='kljafkl;sdfjals;fdj'
+                    title='1426'
+                    body='Sir Ewen Cameron is born.'
                     />
 
                 <TLEvent
-                    title='2000'
-                    body='kljafkl;sdfjals;fdj'
+                    title='1505'
+                    body='Ewen Cameron of Lochiel becomes a knight.'
                     />
                 
                 <TLEvent
-                    title='2100'
-                    body='kljafkl;sdfjals;fdj'
+                    title='1511'
+                    body='Ewen Cameron died.'
                     />
+
+                <TLTimeline
+                    title="Star League \ 2005"
+                    endTitle="2780"
+                    lineStyle={{borderColor:"blue", borderWidth:3}}
+                    >
+
+<TLEvent
+                    title='2118'
+                    body="Torrent Heavy Bomber's Jump Infantry carrying variant enters service."
+                    />
+
+                    </TLTimeline>
 
 
 
