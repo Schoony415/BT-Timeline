6b9d0da816af13060336c2d4da2030ab8a8ddb29
Joshua Schoonover <joshua.schoonover@austincc.edu>
2024-10-24 12:33:28 -0400

    collapse events

--- src/timeline/components/TLEvent.tsx
+++ src/timeline/components/TLEvent.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/TLEvent.css'
 
 
 // a holder to style events from the line
 export default function TLEvent(TLEventProps:{title:String, body:string}){
+    const [showBody, setShowBody] = useState(false);
+
+    const toggleShowBody = () => {setShowBody(!showBody)}
 
     return (
         <div className='TLEvent'>
-            {TLEventProps.title}
-            {/* <p>{TLEventProps.body}</p> */}
+            <div onClick={toggleShowBody} style={{cursor:"pointer"}}>{TLEventProps.title}</div>
+
+            {showBody&&    
             <div dangerouslySetInnerHTML={{__html: TLEventProps.body}}></div>
+            }
         </div>
+
     )
 
 }
